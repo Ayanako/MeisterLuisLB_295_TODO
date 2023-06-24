4796a35840bda7180ef11c8ef719642f8b7b61c8
Luis Meister » <luismeister@gmx.ch >
2023-06-24 20:12:49 +0200

    Projektdateien hinzufügen.

---
+++ MeisterLuisLB_295_TODO/Controllers/TODOController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MeisterLuisLB_295_TODO.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TODOController : ControllerBase
+    {
+        [HttpGet(Name = "GetInteger")]
+        public int Get()
+        {
+            return 100;
+        }
+    }
+}
